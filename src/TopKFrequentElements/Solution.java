729f347affb63bbd6d0e364bee3c2a080a4f2f30
Zahid Esad <116666407+zahidesad@users.noreply.github.com>
2024-12-14 19:17:06 +0300

    "Top K Frequent Elements" solved

--- src/TopKFrequentElements/Solution.java
+++ src/TopKFrequentElements/Solution.java
@@ -1,5 +1,35 @@
 package TopKFrequentElements;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
+public class Solution {
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+
+        PriorityQueue<Map.Entry<Integer, Integer>> topKElements = new PriorityQueue<>(
+                (e1, e2) -> e1.getValue() - e2.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            topKElements.add(entry);
+            if (topKElements.size() > k) {
+                topKElements.poll();
+            }
+        }
+
+        int[] topNumbers = new int[k];
+
+        int i = 0;
+        while (!topKElements.isEmpty()) {
+            topNumbers[i] = topKElements.poll().getKey();
+            i++;
+        }
+
+        return topNumbers;
+    }
 }
--- src/TopKFrequentElements/Test.java
+++ src/TopKFrequentElements/Test.java
@@ -1,7 +1,14 @@
 package TopKFrequentElements;
 
+import static TopKFrequentElements.Solution.topKFrequent;
+
 public class Test {
     public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        int[] result = topKFrequent(nums, 2);
 
+        for (int j : result) {
+            System.out.println(j);
+        }
     }
 }
